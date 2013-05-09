669d6a5391f8c67923763cc7d74227bd7e8e5ab2
Aitor Oses <aitor.oses@gmail.com>
2013-05-10 01:02:13 +0200

    nuevo comienzo

--- application/views/home/default.blade.php
+++ application/views/home/default.blade.php
@@ -40,6 +40,16 @@
 
 </head>
 <body data-wardrobe={{ $id_wardrobe }}>
+	<nav class="top-bar">
+		<ul class="title-area">
+    <!-- Title Area -->
+    <li class="name">
+      <h1><a href="#">Top Bar Title </a></h1>
+    </li>
+    <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+    <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+  </ul>
+	</nav>
 	<!-- POPUP Interior Division  -->
 	<div id="page_screen"></div>
 	<div id="popup" style="display: none;"></div>
