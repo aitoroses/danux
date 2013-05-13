dddbe23a8c11a6686dcac4abb78fcde0a72b9078
Dani <nemz00@gmail.com>
2013-05-13 21:38:20 +0200

    * Mejora en el welcome 	-Añadidos cuadros con mensajes (se pueden quitar si no hay nada que poner, de momento los he puesto por que no e logrado hacer un sticky footer)

--- application/views/home/footer.blade.php
+++ application/views/home/footer.blade.php
@@ -1,5 +1,24 @@
-	<footer>
-    	<div class="small-11 small-centered columns">
-			Semiems © 2013
-		</div>
-	</footer>
+<div class="row">
+	<hr />
+	<div class="full-width">
+      <div class="large-4 columns">
+        <h6><strong>Semiems</strong></h6>
+        <p>Nueva pagina web de  <a href="http://www.armariossemiems.es//">Semiems</a></p>
+      </div>
+
+      <div class="large-4 columns">
+        <h6><strong>Using Foundation?</strong></h6>
+        <p>Let us know how you're using Foundation and we might feature you as an example! <a href="mailto:foundation@zurb.com?subject=I'm%20using%20Foundation">Get in touch →</a></p>
+      </div>
+
+      <div class="large-4 columns">
+        <h6><strong>Versiones</strong></h6>
+        <p>Ya esta liberada la v0.8 de la aplicacion. Puede ver el changelog aqui</p>
+      </div>
+    </div>
+</div>
+<footer>
+	<div class="small-11 small-centered columns">
+		Copyright © Semiems 2013
+	</div>
+</footer>
